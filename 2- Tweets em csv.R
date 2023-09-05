79f43e16ffc7ba0098ad0b0218cf05ce78cd1af4
mauricio-rochalima <65671935+mauricio-rochalima@users.noreply.github.com>
2023-09-05 14:39:50 -0200

    Update 2- Tweets em csv.R

--- 2- Tweets em csv.R
+++ 2- Tweets em csv.R
@@ -4,46 +4,39 @@
 
 tweets <- bind_tweets("/Users/mauriciofernandes/Downloads/Redes Sociais", user = FALSE, verbose = TRUE, output_format = NA) 
 
+
+users <- bind_tweets(data_path = "data/", user = TRUE)
+
+
+convert_json("/Users/mauriciofernandes/Downloads/Redes Sociais", output_format = "tidy")
+
+
+
+# bind json files in the directory "data" into a "tidy" data frame / tibble 
+tweets <- bind_tweets(data_path = "data/", user = TRUE, output_format = "tidy")
+
+
+
+write.csv(tweets,"/Users/mauriciofernandes/Downloads/Redes Sociais\\T2.csv", row.names = FALSE)
+
+
+
+#################################
+
 # Versão Windows 
 
 tweets <- bind_tweets("C:\\Users\\Mauricio\\Downloads\\Redes Sociais", user = FALSE, verbose = TRUE, output_format = NA) 
 
 
-
-# Versão  MAC
-
-users <- bind_tweets(data_path = "data/", user = TRUE)
-
-
-# Versão  Window
-
 users <- bind_tweets(data_path = "C:\\Users\\Mauricio\\Downloads\\Redes Sociais", user = TRUE)
 
-
-
-# Versão  MAC
-convert_json("/Users/mauriciofernandes/Downloads/Redes Sociais", output_format = "tidy")
-
-# Versão  Window
 
 convert_json("C:\\Users\\Mauricio\\Downloads\\Redes Sociais", output_format = "tidy")
 
 
-# Versão  MAC
-
-# bind json files in the directory "data" into a "tidy" data frame / tibble 
-tweets <- bind_tweets(data_path = "data/", user = TRUE, output_format = "tidy")
-
-# Versão  Window
-
 tweets <- bind_tweets(data_path = "C:\\Users\\Mauricio\\Downloads\\Redes Sociais", user = TRUE, output_format = "tidy")
 
-# Versão  MAC
-
-write.csv(tweets,"/Users/mauriciofernandes/Downloads/Redes Sociais\\T2.csv", row.names = FALSE)
-
-
-# Versão  Window
 
 write.csv(tweets,"C:\\Users\\Mauricio\\Downloads\\Redes Sociais\\T2.csv", row.names = FALSE)
+
 
