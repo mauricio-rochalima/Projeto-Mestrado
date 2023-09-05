eaca5178c318287ab6e0d4ab15717f46689e8549
mauricio-rochalima <65671935+mauricio-rochalima@users.noreply.github.com>
2023-09-05 14:37:02 -0200

    Update 2- Tweets em csv.R

--- 2- Tweets em csv.R
+++ 2- Tweets em csv.R
@@ -10,8 +10,16 @@ tweets <- bind_tweets("C:\\Users\\Mauricio\\Downloads\\Redes Sociais", user = FA
 
 
 
+# Versão  MAC
 
 users <- bind_tweets(data_path = "data/", user = TRUE)
+
+
+# Versão  Window
+
+users <- bind_tweets(data_path = "C:\\Users\\Mauricio\\Downloads\\Redes Sociais", user = TRUE)
+
+
 
 # Versão  MAC
 convert_json("/Users/mauriciofernandes/Downloads/Redes Sociais", output_format = "tidy")
@@ -21,10 +29,21 @@ convert_json("/Users/mauriciofernandes/Downloads/Redes Sociais", output_format =
 convert_json("C:\\Users\\Mauricio\\Downloads\\Redes Sociais", output_format = "tidy")
 
 
+# Versão  MAC
 
 # bind json files in the directory "data" into a "tidy" data frame / tibble 
 tweets <- bind_tweets(data_path = "data/", user = TRUE, output_format = "tidy")
 
+# Versão  Window
 
+tweets <- bind_tweets(data_path = "C:\\Users\\Mauricio\\Downloads\\Redes Sociais", user = TRUE, output_format = "tidy")
+
+# Versão  MAC
 
 write.csv(tweets,"/Users/mauriciofernandes/Downloads/Redes Sociais\\T2.csv", row.names = FALSE)
+
+
+# Versão  Window
+
+write.csv(tweets,"C:\\Users\\Mauricio\\Downloads\\Redes Sociais\\T2.csv", row.names = FALSE)
+
