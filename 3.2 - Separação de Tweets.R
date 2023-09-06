3ddaf5289007f8406132e1a3cb8108278ed7ee55
mauricio-rochalima <65671935+mauricio-rochalima@users.noreply.github.com>
2023-09-06 13:58:38 -0300

    Create 3.2 - Separação de Tweets.R

---
+++ 3.2 - Separação de Tweets.R
@@ -0,0 +1,17 @@
+library(dplyr)
+
+# Classifica os tweets
+classified_tweets <- tweets %>%
+  mutate(tweet_category = case_when(
+    grepl("^@\\w+", tweets$text) ~ "mention",        # Menção
+    grepl("^RT @\\w+", tweets$text) ~ "retweet",     # Retweet
+    grepl("^@\\w+", tweets$text) ~ "reply",          # Resposta
+    TRUE ~ "tweet"                                  # Tweet
+  ))
+
+
+filter(classified_tweets,tweet_category!="tweet")
+
+
+# Exibe o resultado
+print(classified_tweets)
