4150e4e50cca7bb8f3bb204a44164db2b1059176
mauricio-rochalima <65671935+mauricio-rochalima@users.noreply.github.com>
2023-09-06 16:07:43 -0300

    Update 2- Tweets em csv.R

--- 2- Tweets em csv.R
+++ 2- Tweets em csv.R
@@ -5,7 +5,7 @@
 tweets <- bind_tweets("/Users/mauriciofernandes/Downloads/Redes Sociais", user = FALSE, verbose = TRUE, output_format = NA) 
 
 
-users <- bind_tweets(data_path = "data/", user = TRUE)
+users <- bind_tweets(data_path = "/Users/mauriciofernandes/Downloads/Redes Sociais", user = TRUE)
 
 
 convert_json("/Users/mauriciofernandes/Downloads/Redes Sociais", output_format = "tidy")
@@ -13,7 +13,7 @@ convert_json("/Users/mauriciofernandes/Downloads/Redes Sociais", output_format =
 
 
 # bind json files in the directory "data" into a "tidy" data frame / tibble 
-tweets <- bind_tweets(data_path = "data/", user = TRUE, output_format = "tidy")
+tweets <- bind_tweets(data_path = "/Users/mauriciofernandes/Downloads/Redes Sociais", user = TRUE, output_format = "tidy")
 
 
 
